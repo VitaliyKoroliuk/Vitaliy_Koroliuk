38bb6ae4435502252513d69328335695901adf0b
Vitaliy Koroliuk <vitalii.koroliuk@nure.ua>
2017-10-15 23:10:47 +0300

    Initial commit for usermanagement

---
+++ usermanagement/src/main/java/koroliukkn155/User.java
@@ -0,0 +1,47 @@
+package koroliukkn155;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firsName) {
+		this.firstName = firsName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getFullName() {
+		return getLastName() + ", " + getFirstName();
+	}
+	public Object getAge() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		int currentYear = calendar.get(Calendar.YEAR);
+		calendar.setTime(getDateOfBirth());
+		int year = calendar.get(Calendar.YEAR);
+		return currentYear - year;
+	}
+	
+}
